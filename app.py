56377fd5c5516b36d61ae37f2927c6e548fc17d7
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-24 11:47:43 -0500

    created some simple page navigation

--- app.py
+++ app.py
@@ -1,52 +1,9 @@
-import pandas as pd
 import streamlit as st
-from utils.utils import *
 
+upload_page = st.Page(page="pages/upload.py", title="Upload", icon="⬆️")
+grouping_page = st.Page(page="pages/grouping.py", title="Grouping", icon="📦")
+report_page = st.Page(page="pages/report.py", title="Report", icon="📊")
 
-st.title("Manual Journal Entry Transaction Upload")
-uploaded_file = st.file_uploader(
-    "Upload a CSV from the Query Report MANUAL_JOURNAL_TRANSACTION_ENTRY",
-    type="csv",
-    accept_multiple_files=False,
-)
+pg = st.navigation(pages=[upload_page, grouping_page, report_page])
 
-if uploaded_file is not None:
-    raw = pd.read_csv(uploaded_file, dtype=str)
-    st.subheader("Raw Uploaded Data")
-    st.dataframe(raw.head())
-
-    st.subheader("Raw Data Types")
-    dtypes_df = pd.DataFrame(raw.dtypes, columns=["Data Type"])
-    st.dataframe(dtypes_df, height=200)
-
-    try:
-        st.subheader("Clean Data")
-        clean_data = clean_data(raw)
-        st.dataframe(clean_data)
-    except Exception as e:
-        st.subheader("Data Cleaning Error")
-        st.error(f"{e}")
-
-    if st.button("Ingest Data", icon="🚀"):
-        st.session_state.show_confirm = True
-
-    if st.session_state.get("show_confirm", False):
-        st.warning("Are you sure you want to ingest the data?")
-        col1, col2 = st.columns(2)
-        with col1:
-            if st.button("✅ Yes, proceed"):
-                st.success("Data ingestion started!")
-
-                try:
-                    # drop all rows from the minimum date from supplied rows
-                    drop_data_from_minimum_date_created(clean_data)
-                    insert_data(clean_data.to_dict(orient="records"))
-                except Exception as e:
-                    st.subheader("Data Ingestion Error")
-                    st.error(f"{e}")
-
-                st.session_state.show_confirm = False  # Reset state
-        with col2:
-            if st.button("❌ Cancel"):
-                st.info("Action cancelled.")
-                st.session_state.show_confirm = False  # Reset state
+pg.run()
