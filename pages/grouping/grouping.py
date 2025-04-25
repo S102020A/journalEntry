3cc4e20202f60e6d42081003dc32689149ccd3e5
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-25 13:29:16 -0500

    creating the yaml groupings

--- pages/grouping/grouping.py
+++ pages/grouping/grouping.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import yaml
 from utils.db_manager import get_connection
 
 # Connect to the database
@@ -9,42 +10,42 @@ cursor = conn.cursor()
 cursor.execute("SELECT id, name FROM finance.grouping ORDER BY created_at DESC;")
 results = cursor.fetchall()
 
-# Start form
-with st.form("category_form"):
-    st.subheader("Select or Create Category")
+template_data = {
+    "ALL_ACCOUNTS": [
+        {"ASSETS": ["NEW_ACCOUNT_GROUPING"]},
+        "TOTAL_LIABILITIES",
+        "NET ASSETS",
+    ]
+}
 
-    if results:
-        # Create a dictionary of id: name pairs
-        options = {row[0]: row[1] for row in results}
 
-        selected_id = st.selectbox(
-            "Choose a category:",
-            options.keys(),
-            format_func=lambda x: options[x],
-        )
-    else:
-        st.selectbox("Choose a category:", ["No categories available"], disabled=True)
-        selected_id = None
+yaml_string = yaml.dump(template_data, sort_keys=False)
 
-    # Allow user to create a new category
-    new_category = st.text_input("Or create a new category:")
+st.download_button(
+    label="Download YAML Template",
+    data=yaml_string,
+    file_name="template.yaml",
+    mime="text/yaml",
+)
 
-    # Submit button
+st.code(yaml_string, language="yaml")
+
+with st.form("New Grouping"):
+    st.write("Inside form")
+
+    group_name = st.text_input("Name of Grouping")
+    dimension = st.selectbox(
+        "Select dimension", options=["account", "business_unit", "rad"]
+    )
+    uploaded_file = st.file_uploader(label="Json uploader", type="json")
+
     submitted = st.form_submit_button("Submit")
 
     if submitted:
-        if new_category:
-            # Insert new category into the database
-            cursor.execute(
-                "INSERT INTO finance.grouping (name) VALUES (%s) RETURNING id;",
-                (new_category,),
-            )
-            conn.commit()
-            new_id = cursor.fetchone()[0]
-            st.success(f"New category '{new_category}' created with ID {new_id}")
-        elif selected_id:
-            st.success(
-                f"Selected Category ID: {selected_id}, Name: {options[selected_id]}"
-            )
+        st.write("Form submitted!")
+        st.write("Group Name:", group_name)
+        st.write("Dimension:", dimension)
+        if uploaded_file is not None:
+            st.write("Uploaded File Name:", uploaded_file.name)
         else:
-            st.warning("Please select or create a category.")
+            st.write("No file uploaded.")
