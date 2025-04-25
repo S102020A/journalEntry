aa539c61e358393f1630c66aa3f7a30706289de5
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-25 10:15:26 -0500

    dropped the hierarchy sql functions as they are very complicated. The master trial balance is working as expected

--- schema/func.sql
+++ schema/func.sql
@@ -1,159 +1,3 @@
-CREATE OR REPLACE FUNCTION finance.get_account_hierarchy(account_no TEXT)
-RETURNS TABLE
-(
-    "is_input_id_root" BOOLEAN,
-    "id" INT,
-    "parent" TEXT,
-    "child" TEXT
-)
-AS $$
-BEGIN
-    RETURN QUERY (
-        WITH RECURSIVE
-        child_hierarchy AS (
-            SELECT
-                FALSE as is_input_id_root,
-                x.id,
-                x.account_no as child,
-                x.parent_account_no as parent
-            FROM finance.account_ownership x
-            WHERE
-                parent_account_no = $1
-            UNION ALL
-            SELECT
-                FALSE as is_input_id_root,
-                y.id,
-                y.account_no as child,
-                y.parent_account_no as parent
-            FROM finance.account_ownership y
-            JOIN child_hierarchy ch ON y.parent_account_no = ch.child
-        )
-        SELECT
-            z.is_input_id_root,
-            z.id,
-            z.parent,
-            z.child
-        FROM child_hierarchy z
-    );
-
-    IF NOT FOUND THEN
-    RETURN QUERY (
-        SELECT
-            TRUE as is_input_id_root,
-            not_found.id,
-            not_found.parent_account_no as parent,
-            not_found.account_no as child
-        FROM finance.account_ownership not_found
-        WHERE not_found.account_no = $1
-    );
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION finance.get_business_unit_hierarchy(business_unit_id TEXT)
-RETURNS TABLE
-(
-    "is_input_id_root" BOOLEAN,
-    "id" INT,
-    "parent" TEXT,
-    "child" TEXT
-)
-AS $$
-BEGIN
-    RETURN QUERY (
-        WITH RECURSIVE
-        child_hierarchy AS (
-            SELECT
-                FALSE as is_input_id_root,
-                x.id,
-                x.business_unit_id as child,
-                x.parent_business_unit_id as parent
-            FROM finance.business_unit_ownership x
-            WHERE
-                x.parent_business_unit_id = $1
-            UNION ALL
-            SELECT
-                FALSE as is_input_id_root,
-                y.id,
-                y.business_unit_id as child,
-                y.parent_business_unit_id as parent
-            FROM finance.business_unit_ownership y
-            JOIN child_hierarchy ch ON y.parent_business_unit_id = ch.child
-        )
-        SELECT
-            z.is_input_id_root,
-            z.id,
-            z.parent,
-            z.child
-        FROM child_hierarchy z
-    );
-
-    IF NOT FOUND THEN
-    RETURN QUERY (
-        SELECT
-            TRUE as is_input_id_root,
-            not_found.id,
-            not_found.parent_business_unit_id as parent,
-            not_found.business_unit_id as child
-        FROM finance.business_unit_ownership not_found
-        WHERE not_found.business_unit_id = $1
-    );
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION finance.get_rad_hierarchy(rad_id TEXT)
-RETURNS TABLE
-(
-    "is_input_id_root" BOOLEAN,
-    "id" INT,
-    "parent" TEXT,
-    "child" TEXT
-)
-AS $$
-BEGIN
-    RETURN QUERY (
-        WITH RECURSIVE
-        child_hierarchy AS (
-            SELECT
-                FALSE as is_input_id_root,
-                x.id,
-                x.rad_id as child,
-                x.rad_type_id as parent
-            FROM finance.rad x
-            WHERE
-                x.rad_type_id = $1
-            UNION ALL
-            SELECT
-                FALSE as is_input_id_root,
-                y.id,
-                y.rad_id as child,
-                y.rad_type_id as parent
-            FROM finance.rad y
-            JOIN child_hierarchy ch ON y.rad_type_id = ch.child
-        )
-        SELECT
-            z.is_input_id_root,
-            z.id,
-            z.parent,
-            z.child
-        FROM child_hierarchy z
-    );
-
-    IF NOT FOUND THEN
-    RETURN QUERY (
-        SELECT
-            TRUE as is_input_id_root,
-            not_found.id,
-            not_found.rad_type_id as parent,
-            not_found.rad_id as child
-        FROM finance.rad not_found
-        WHERE not_found.rad_id = $1
-    );
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION finance.get_fiscal_year(input_date DATE)
 RETURNS INTEGER AS $$
 DECLARE
@@ -191,7 +35,7 @@ BEGIN
             SUM(CASE WHEN j.accounting_date BETWEEN start_date AND end_date THEN j.amount ELSE 0 END)::DECIMAL(20,2) AS activity_balance,
             SUM(j.amount)::DECIMAL(20,2) AS closing_balance,
             COUNT(*) FILTER (WHERE j.accounting_date BETWEEN start_date AND end_date) AS transaction_count
-        FROM finance.journal_entry j
+        FROM finance.manual_journal_entry_transaction j
         WHERE j.accounting_date <= end_date
         GROUP BY j.account_no, j.business_unit_id
     )
@@ -228,7 +72,7 @@ BEGIN
             SUM(CASE WHEN j.accounting_date BETWEEN start_date AND end_date THEN j.amount ELSE 0 END) AS activity_balance,
             SUM(CASE WHEN j.accounting_date <= end_date THEN j.amount ELSE 0 END) AS closing_balance,
             COUNT(*) FILTER (WHERE j.accounting_date BETWEEN start_date AND end_date) AS transaction_count
-        FROM finance.journal_entry j
+        FROM finance.manual_journal_entry_transaction j
         WHERE j.accounting_date <= end_date
         GROUP BY j.account_no, j.business_unit_id, j.rad_data
     )
@@ -324,7 +168,7 @@ BEGIN
             0::NUMERIC as closing_balance,  -- there are no balances for budget data
             count(*) as transaction_count,  
             'CurrentFYBudget' AS time_period
-        FROM finance.budget bgt 
+        FROM finance.manual_budget bgt 
         WHERE 
             bgt.fiscal_year >= finance.get_fiscal_year(anchor_date)
         GROUP BY
@@ -351,7 +195,7 @@ BEGIN
         ) AS je
 
     ) AS master
-    JOIN finance."account" a ON master.account_no = a.account_no
-    JOIN finance.business_unit b ON master.business_unit_id = b.business_unit_id;
+    JOIN finance.vw_account a ON master.account_no = a.account_no
+    JOIN finance.vw_business_unit b ON master.business_unit_id = b.business_unit_id;
 END;
 $$ LANGUAGE plpgsql;
--- schema/views.sql
+++ schema/views.sql
@@ -1,4 +1,26 @@
-SELECT DISTINCT
-    "ACCOUNT_NO" AS ACCOUNT
-FROM FINANCE."MANUAL_JOURNAL_ENTRY_TRANSACTION"
-GROUP BY "ACCOUNT_NO"
+CREATE VIEW finance.vw_account AS
+SELECT 
+    account_no, 
+    account, 
+    MAX(account_type) AS account_type
+FROM (
+    SELECT DISTINCT account_no, account, '' AS account_type 
+    FROM manual_budget
+
+    UNION ALL
+
+    SELECT DISTINCT account_no, account, account_type 
+    FROM manual_journal_entry_transaction
+) temp
+GROUP BY account_no, account
+ORDER BY account_no, account;
+
+
+CREATE VIEW finance.vw_business_unit AS
+SELECT DISTINCT business_unit_id, business_unit
+FROM (
+    SELECT DISTINCT business_unit_id, business_unit FROM manual_budget
+    UNION ALL
+    SELECT DISTINCT business_unit_id, business_unit FROM manual_journal_entry_transaction
+) temp
+ORDER BY business_unit_id, business_unit;
