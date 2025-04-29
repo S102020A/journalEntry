28b29ff2c241eb7108dfa072d6c301ff67358e4a
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-29 08:48:00 -0500

    got the crud operations working

--- pages/grouping/grouping.py
+++ pages/grouping/grouping.py
@@ -25,63 +25,89 @@ st.download_button(
 
 st.code(json_string, language="json")
 
-
 create, read, update, delete = st.tabs(["✍️ Create", "📖 Read", "✏️ Update", "🗑️ Delete"])
 
-
 with create:
-    with st.form("New Grouping"):
-        st.write("Insert New Grouping")
+    try:
+        with st.form("New Grouping"):
+            st.write("Insert New Grouping")
 
-        group_name = st.text_input("Name of Grouping")
-        created_by = st.text_input("Created By")
-        dimension = st.selectbox(
-            "Select dimension", options=["account", "business_unit"]
-        )
-        uploaded_json_file = st.file_uploader(label="Json uploader", type="json")
-        submitted = st.form_submit_button("Submit")
-
-        if submitted and validate_form(
-            group_name, created_by, dimension, uploaded_json_file
-        ):
-            parsed_json = json.load(uploaded_json_file)
-            json_str = json.dumps(parsed_json)
-            cursor.execute(
-                """
-                    INSERT INTO finance.grouping (name, dimension, grouping, created_by) VALUES (%s, %s, %s, %s)
-                """,
-                (group_name, dimension, json_str, created_by),
+            group_name = st.text_input("Name of Grouping")
+            created_by = st.text_input("Created By")
+            dimension = st.selectbox(
+                "Select dimension", options=["account", "business_unit"]
             )
-            conn.commit()
+            uploaded_json_file = st.file_uploader(label="Json uploader", type="json")
+            submitted = st.form_submit_button("Submit")
 
-            st.success("Grouping successfully inserted into the database!")
+            if submitted and validate_form(
+                group_name, created_by, dimension, uploaded_json_file
+            ):
+                parsed_json = json.load(uploaded_json_file)
+                json_str = json.dumps(parsed_json)
+                cursor.execute(
+                    """
+                        INSERT INTO finance.grouping (name, dimension, grouping, created_by) VALUES (%s, %s, %s, %s)
+                    """,
+                    (group_name, dimension, json_str, created_by),
+                )
+                conn.commit()
+
+                st.success("Grouping successfully inserted into the database!")
+    except Exception as error:
+        st.error(error)
 
 with read:
-    query = """
-            SELECT id, name, grouping::TEXT, dimension, created_by
-            FROM finance.grouping
-            ORDER BY created_at DESC;
-        """
-    cursor.execute(query)
-    data = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    raw_data = pd.DataFrame(data, columns=columns)
-    grid_response = get_read_ag_grid(raw_data)
+    try:
+        query = """
+                SELECT id, name, grouping::TEXT, dimension, created_by
+                FROM finance.grouping
+                ORDER BY created_at DESC;
+            """
+        cursor.execute(query)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        raw_data = pd.DataFrame(data, columns=columns)
+        grid_response = get_read_ag_grid(raw_data)
+    except Exception as error:
+        st.error(error)
 
 with update:
-    query = """
-            SELECT id, name, grouping::TEXT, dimension, created_by
-            FROM finance.grouping
-            ORDER BY created_at DESC;
-        """
-    cursor.execute(query)
-    data = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    raw_data = pd.DataFrame(data, columns=columns)
-    grid_response = get_update_ag_grid(raw_data)
+    try:
+        query = """
+                SELECT id, name, dimension, created_by
+                FROM finance.grouping
+                ORDER BY created_at DESC;
+            """
+        cursor.execute(query)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        raw_data = pd.DataFrame(data, columns=columns)
+        grid_response = get_update_ag_grid(raw_data)
+
+        # obtain in a fram the updated dataframe
+        updated_data = pd.DataFrame(grid_response["data"]).reset_index(drop=True)
+        original_data = raw_data.reset_index(drop=True)
+
+        if not updated_data.equals(original_data):
+            changes = updated_data.compare(
+                original_data, keep_shape=True, keep_equal=True
+            )
+            modified_indices = changes.dropna(how="all").index.tolist()
+
+            if modified_indices:
+                st.success(f"Updating {len(modified_indices)} modified row(s)...")
+
+                for i in modified_indices:
+                    update_row_in_database(updated_data.loc[i].to_dict())
+
+                st.info("Data updated in DB. Refreshing...")
+                st.rerun()
+    except Exception as error:
+        st.error(error)
 
 with delete:
-    with st.container():
+    try:
         # Fetch data
         query = """
             SELECT id, name, grouping::TEXT, dimension, created_by
@@ -99,7 +125,7 @@ with delete:
         # Check if a row is selected
         selected = grid_response["selected_rows"]
         if selected is not None:
-            selected_id = int(selected.iloc[0]["ID"])
+            selected_id = int(selected.iloc[0]["id"])
             if st.button(f"Delete Selected ID {selected_id}"):
                 cursor.execute(
                     "DELETE FROM finance.grouping WHERE id = %s;", (selected_id,)
@@ -107,3 +133,5 @@ with delete:
                 conn.commit()
                 st.success(f"Deleted row with ID {selected_id}")
                 st.rerun()
+    except Exception as error:
+        st.error(error)
--- utils/ag_grid.py
+++ utils/ag_grid.py
@@ -1,17 +1,17 @@
 import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
 custom_css = {
     ".ag-root": {"background-color": "#0e1117"},
     ".ag-header": {"background-color": "#0e1117"},
     ".ag-cell": {"background-color": "#0e1117"},
     ".ag-row": {"background-color": "#0e1117"},
+    ".ag-paging-panel": {"background-color": "#0e1117"},
 }
 
 
 def get_delete_ag_grid(raw_data: pd.DataFrame):
     # Build grid options
-    raw_data.columns = raw_data.columns.str.upper()
     gb = GridOptionsBuilder.from_dataframe(raw_data)
     gb.configure_selection(selection_mode="single", use_checkbox=True)
     gb.configure_pagination(enabled=True, paginationAutoPageSize=10)
@@ -33,11 +33,10 @@ def get_delete_ag_grid(raw_data: pd.DataFrame):
 
 def get_update_ag_grid(raw_data: pd.DataFrame):
     # Build grid options
-    raw_data.columns = raw_data.columns.str.upper()
     gb = GridOptionsBuilder.from_dataframe(raw_data)
     gb.configure_pagination(enabled=True, paginationAutoPageSize=10)
     gb.configure_default_column(filter=True, editable=True)
-    gb.configure_column("ID", editable=False)
+    gb.configure_column("id", editable=False)
     grid_options = gb.build()
 
     # Display interactive grid
@@ -46,6 +45,7 @@ def get_update_ag_grid(raw_data: pd.DataFrame):
         gridOptions=grid_options,
         height=400,
         fit_columns_on_grid_load=True,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
         allow_unsafe_jscode=True,
         custom_css=custom_css,
     )
@@ -55,7 +55,6 @@ def get_update_ag_grid(raw_data: pd.DataFrame):
 
 def get_read_ag_grid(raw_data: pd.DataFrame):
     # Build grid options
-    raw_data.columns = raw_data.columns.str.upper()
     gb = GridOptionsBuilder.from_dataframe(raw_data)
     gb.configure_pagination(enabled=True, paginationAutoPageSize=10)
     gb.configure_default_column(editable=False)
